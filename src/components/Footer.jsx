5f086bceadd7a8e10b59ca28c20c24dde08f4e89
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2023-10-13 01:59:59 +0530

    footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const Footer = () => {
   return (
-    <div className='px-4'>
+    <div className='px-4 absolute bottom-0 w-full'>
       <hr className="border-b-2 mx-2 mt-3 inline-block border-[#377aff] w-full" />
       <div className="flex justify-between">
         <p className="font-bold pl-1 text-lg text-[#1463FF]">
